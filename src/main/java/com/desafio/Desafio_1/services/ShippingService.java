65b7e8171009e70549cbfeed44e480711ad9bf27
Victor Da Costa Vasco <victordcvasco@gmail.com>
2025-02-17 10:14:51 -0300

    Shipping method update

--- src/main/java/com/desafio/Desafio_1/services/ShippingService.java
+++ src/main/java/com/desafio/Desafio_1/services/ShippingService.java
@@ -7,14 +7,24 @@ import com.desafio.Desafio_1.entities.Order;
 @Service
 public class ShippingService {
 
+//	public Double Shipment(Order order) {
+//		if(order.getBasic() < 100) {
+//			order.setBasic(order.getBasic() - order.getBasic() + 20);
+//		} else if(order.getBasic() >= 100 && order.getBasic() <= 200) {
+//			order.setBasic(order.getBasic() - order.getBasic() + 12);
+//		} else {
+//			order.setBasic(order.getBasic() - order.getBasic() + 0);
+//		}
+//		return order.getBasic();
+//	}
+	
 	public Double Shipment(Order order) {
 		if(order.getBasic() < 100) {
-			order.setBasic(order.getBasic() - order.getBasic() + 20);
+			return 20.0;
 		} else if(order.getBasic() >= 100 && order.getBasic() <= 200) {
-			order.setBasic(order.getBasic() - order.getBasic() + 12);
+			return 12.0;
 		} else {
-			order.setBasic(order.getBasic() - order.getBasic() + 0);
+			return 0.0;
 		}
-		return order.getBasic();
 	}
 }
